ada9c5d140c374f8d49da1cce432af3250697464
Jia Long Xiao <gary.xiao94@gmail.com>
2019-12-29 21:22:40 +1100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jia Long Xiao on 29/12/19.
+//  Copyright © 2019 Jia Long Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
